04363af3327c7b3e75aa83bf221046a3968a5f20
Roland Li <roland_li@hotmail.ca>
2018-06-04 16:36:31 -0400

    Just some comments

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Checks SONA instance for new studies via ID check. Tracks encountered IDs and sends email alerts for new ones.
 
 How to use:
-Open up sona-checker.sh; comments should tell you which files you'll need to define. 
+Open up sona-checker.sh; comments should tell you which files you'll need to define.
+<br> 
 Requires credentials to login to SONA, email address for alerts.
-To set up perpetutal check, use crontab or scheduler.
+<br
+To set up continuous checking, use crontab or service.
--- sona-checker.sh
+++ sona-checker.sh
@@ -3,23 +3,32 @@
 #Quick 'N Dirty checker for sona
 #To be scheduled by crontab 
 
-#Defining permenant files that will be used
-ID_FILE="ids.txt"
-COOKIE_FILE="cookies.txt"
-EMAILS_FILE="email_list.txt"
+#_______________________________________________________________________
+# USER OPTIONS
+
+#These files must be user defined
+#1: Emails file; 1 email address per line. Sends emails to these.
+#2: Credentials file; line 1 is username, line 2 is password
+EMAILS_FILE="email_list.txt" 
+CREDENTIALS_FILE="credentials.txt" 
 
 #Your page to check; modify to your sona instance
 #Can technically only use the one page but...
 DEFAULT_PAGE="https://wlu-ls.sona-systems.com/default.aspx"
 STUDIES_PAGE="https://wlu-ls.sona-systems.com/all_exp_participant.aspx"
 
+#These files will be generated to keep track
+ID_FILE="ids.txt"
+COOKIE_FILE="cookies.txt"
+#_______________________________________________________________________
+
 login() {
     #Default session id
     SESSIONID='251l33svkwftvv34ytpsl5p1'
 
     #Grab credentials
-    USERNAME=$(sed '1q;d' credentials.txt)
-    PASSWORD=$(sed '2q;d' credentials.txt)
+    USERNAME=$(sed '1q;d' $CREDENTIALS_FILE)
+    PASSWORD=$(sed '2q;d' $CREDENTIALS_FILE)
 
     #Fields to pass to curl
     DATA="__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUJNjM2MDQ1NTgyZGSHi2XgX%2FUE1%2F9%2BPCRY6NJ4nXYVWCVXfAYIFvqAveLC3A%3D%3D&__VIEWSTATEGENERATOR=CA0B0334&__EVENTTARGET=&__EVENTARGUMENT=&__EVENTVALIDATION=%2FwEdAAVJNJYm0f4uWg7cS6joTPw5UIlPJ3shF6ZfHx5cHAdswX1Gsa4Qp9IFMNZyT1m%2FORlOGPoKvJSxXl507%2BPWyULdk0IaRa81gSyF%2Ft2E7n3iJWU%2BD9YgP8jtn3s5kkIRi4NZc9SrPpif7I8VynwW%2BcCE&ctl00%24ContentPlaceHolder1%24return_experiment_id=&ctl00%24ContentPlaceHolder1%24userid=${USERNAME}&ctl00%24ContentPlaceHolder1%24pw=${PASSWORD}%21&ctl00%24ContentPlaceHolder1%24default_auth_button=Log+In"
